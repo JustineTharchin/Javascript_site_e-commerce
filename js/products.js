f01b0dd4a4ff0e2b4ff6b2b6bb55770abce4b1cc
JustineTharchin <54324028+JustineTharchin@users.noreply.github.com>
2021-09-03 18:42:50 +0200

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -1,5 +1,3 @@
-// Javascript pour l'affichage en liste des produits
-
 //Définition d'une constante pour l'url de la requête GET Api
 const URL = "http://localhost:3000/api/cameras/";
 // initialisation d'une variable idParams qui sera utilisé pour compléter l'url produit et comme "key" pour le stockage dans le session.storage
@@ -18,54 +16,50 @@ if (objectId === null) {
 getApiData = () => {
         return fetch((URL + idParams))
             .then(response => response.json())
-            .then(result => result)
+            .then(result => {
+				console.log(result);
+				displayProduct(result.name, result.price, result.description, result.imageUrl, result.lenses, result._id);
+			})
             .catch(error => {
                 // en cas d'erreur de chargement de l'API affichage d'un message sur l'écran de l'utilisateur + message d'erreur dans la console
                 apiFail = document.querySelector('.bloc2');
-                apiFail.classList.add('fail__msg');
-                apiFail.innerHTML = "Veuillez démarrer le serveur";
                 console.error(error);
             });
 };
 
-//fonction pour l'affichage des produits en liste sur la page index.html
-async function pageProduct() {
-    let cameras = await getApiData();
-    console.table(cameras);
-}
-
-pageProduct();
-
-function displayProduct(name, price, description, imageUrl, id) {
+function displayProduct(name, price, description, imageUrl, lenses, id) {
 
 	//récupération de la chaîne de requête dans l'url
 	const queryString_url_id = window.location.search;
 	console.log(queryString_url_id);
 
-	//enlever le ? qui se trouve avant l'id dans la console: methode 1 extraire juste l'id
 	const reccupId = queryString_url_id.slice(4);
 
-	//affichage du produit selectionné par l'id: avec fetch et en mettant la valeur de l'id à la fin de l'url
-//methode .find()
 	const idProduitSelectionne = parent.find(element => element._id === id);
 	console.log(idProduitSelectionne);
 	const positionElt = document.querySelector(".product_card");
 
+	const lensString = document.querySelector(".options");
+		for(var i = 0; i <lenses.length; i++) {
+		console.log(lenses[i]);
+		}
+	
+
     const pageCamera = `
 	<section class="product_card" id="product_card">
-	<h1>${idProduitSelectionne.name}</h1>
-			<img src="${idProduitSelectionne.imageUrl}" class="product__image" style="width: 500px;">
+	<h1>${name}</h1>
+			<img src="${imageUrl}" class="product__image" style="width: 500px;">
 		<div class="product__description">
-			<h2>${idProduitSelectionne.price} €</h2>
-			<p>${idProduitSelectionne.description}</p>
+			<h2>${price/100} €</h2>
+			<p>${description}</p>
 		</div>
 			<div class="quantity-wrapper">
-					<label for="option" style="margin-right: 0.5rem;">Lenses:</label>
-					<select name="option" style="margin-bottom: 1rem;">
-						<option value disabled>Options<h1>${idProduitSelectionne.lenses}</h1></option>
-						<option value="1" selected>1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
+					<label for="options" style="margin-right: 0.5rem;">Lenses:</label>
+					<select id="options" style="margin-bottom: 1rem;">
+					<option value disabled>Lentilles</option>
+						<option value="1" selected>${lenses[0]}</option>
+						<option value="2">${lenses[1]}</option>
+						<option value="2">${lenses[2]}</option>
 					</select>
 			</div>
 				<button class="btn-go-checkout">
@@ -79,4 +73,35 @@ function displayProduct(name, price, description, imageUrl, id) {
 positionElt.innerHTML = pageCamera;
 }
 
-displayProduct();
+getApiData();
+
+
+
+
+
+
+
+
+
+
+
+
+//const btnCart = document.querySelector(".btn-go-checkout");
+//console.log(btnCart);
+
+//btnCart.addEventListener("click", (event)=>{
+	//event.preventDefault();
+
+//const idOption = document.querySelector("#option");
+
+//const choiceOption = idOption.lenses;
+//console.log(choiceOption);
+
+//reccuperation des valeurs
+
+//let optionsProduct = {
+	//options : choiceOption
+//}
+
+//console.log(optionsProduct);
+//});
